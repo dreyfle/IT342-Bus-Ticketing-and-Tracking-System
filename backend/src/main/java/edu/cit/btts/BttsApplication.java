17311ecc319ca6a8989b887450f1a026db1d7d64
LAB12_USER ON WS14 <L12X15W14@citulab.net>
2025-06-25 15:54:14 +0800

    first commit to repository

---
+++ backend/src/main/java/edu/cit/btts/BttsApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.btts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BttsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BttsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/cit/btts/BttsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.btts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BttsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
